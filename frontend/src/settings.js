394cd86c9f5c2557c9a0c7e2ee325104394ea5b7
imthaghost <imthaghost@protonmail.com>
2019-12-13 11:52:02 -0800

    [update]

--- frontend/src/settings.js
+++ frontend/src/settings.js
@@ -1,6 +1,6 @@
-let DEBUG = true;
-let HOST_URL = "http://3.18.215.82:8000";
-let SOCKET_URL = "http://3.18.215.82:8000";
+let DEBUG = false;
+let HOST_URL = "0.0.0.0:8000";
+let SOCKET_URL = "0.0.0.0:8000";
 if (DEBUG) {
     HOST_URL = "http://127.0.0.1:8000";
     SOCKET_URL = "ws://127.0.0.1:8000";
